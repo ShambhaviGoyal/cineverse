2d4acbb87fd4e0169238b4cfba8707e5b5e961a8
Claire Wang <wangclaire07@gmail.com>
2025-08-02 17:39:53 -0400

    Added UI fixes to movie popups

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.x.x")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
 
     implementation(libs.androidx.core.ktx)
